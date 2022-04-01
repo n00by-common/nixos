b663345198fef97da932da97ac0d1bc587668449
Hannes Bredberg <hannesbredberg@gmail.com>
2022-04-01 13:17:14 +0000

    Add it23 configuration

---
+++ it23.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./common.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking = {
+    useDHCP = false;
+    networkmanager.enable = true;
+    hostName = "it23-nixos";
+    interfaces = {
+      eno1.useDHCP = true;
+      wlp2s0.useDHCP = true;
+    };
+  };
+
+  system.stateVersion = "21.11";
+}
+
